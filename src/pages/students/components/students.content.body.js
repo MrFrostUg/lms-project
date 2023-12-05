1bdb224d627963e166d03142da99743d4d956df3
MrFrostUg <frostmartin882@gmail.com>
2023-12-05 02:11:56 +0100

    deleted unnecesary files

--- src/pages/students/components/students.content.body.js
+++
@@ -1,8 +0,0 @@
-import { generateStudentCardsHTML } from "./student.card.generator.js";
-
-export const studentsBodyContainerFragment = () => {
-  return `<div class="container d-flex justify-content-center  my-2">
-  <div class="row flex-wrap">
-    ${generateStudentCardsHTML()}
-  </div>`;
-};
